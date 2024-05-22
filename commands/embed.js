43512e750bcbde5062161d391b05eb3f892a738b
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-22 08:15:31 +0700

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -8,12 +8,12 @@ module.exports = {
     .setColor(0x0099FF)
     .setTitle('This is Title')
     .setDescription('Some description here')
-    .setThumbnail('https://cdn.discordapp.com/attachments/1113800537402527903/1239609375635673128/Untitled_design.png?ex=664d6ef1&is=664c1d71&hm=3770aa56d53c8ef070a7785b2f5de1a7dc23e47a23cee631729c58fc90e4442b&')
+    .setThumbnail('https://media.discordapp.net/attachments/1090895809098289185/1239931775317315584/tai_xuong_1.gif?ex=664df274&is=664ca0f4&hm=392a09831d3eff9a0098ef571da3c7c24291441edf4aad6933d0ba95eed58d00&')
     .addFields(
       { name: 'Field Name ', value: 'Some value here' },
-       { name: 'GlaceYT', value: 'Subscribe to my channel' },
+       { name: 'Kaioshin', value: 'Join Discord Server' },
     )
-    .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=664dc637&is=664c74b7&hm=4e5df87c272535fbd67b7b33ad97ee82eeac869feede072a6112f71d7f8dbd20&')
+    .setImage('https://media.discordapp.net/attachments/1090895809098289185/1239931775317315584/tai_xuong_1.gif?ex=664df274&is=664ca0f4&hm=392a09831d3eff9a0098ef571da3c7c24291441edf4aad6933d0ba95eed58d00&')
     .setTimestamp()
     .setFooter({ text: 'Some footer text here' });
 
